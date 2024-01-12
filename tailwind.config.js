0176f8edbe4d7afd87017308b1da328bedf488cc
luis <luisfr364@gmail.com>
2024-01-12 14:55:10 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./dist/*.{html,js}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                squares: "url('../assets/squares.png')",
+            },
+            keyframes: {
+                opacity1: {
+                    "0%": { opacity: "0" },
+                    "50%": { opacity: "0" },
+                    "80%": { opacity: "0.75" },
+                    "100%": { opacity: "1" },
+                },
+            },
+            animation: {
+                opacityTransition: "opacity1 1s",
+                opacityTransition2: "opacity1 1.4s",
+            },
+        },
+    },
+    plugins: [],
+};
